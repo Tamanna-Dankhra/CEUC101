f673c045e40e4d90391dff2e9a3cb1e968022916
Tamanna-Dankhra <tamannadankhra3113@gmail.com>
2025-09-09 19:47:20 +0530

    Add files via upload

---
+++ Pr 6/Pr 6.c
@@ -0,0 +1,13 @@
+// This program is about conversion of celsius to fahrenheit
+#include<stdio.h>
+#define celsius =c
+#define fahrenheit =f
+void main(){
+    float c,f;
+    printf("enter celsius value= ");
+    scanf("%f",&c);
+
+    f=(c*(9/5))+32;
+    printf("fahrenheit value= %f\n",f);
+
+}
